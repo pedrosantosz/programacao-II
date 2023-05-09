4d35d9ff4449e6642df1e8c0e1631046c9b118ba
Pedro Santos <89491903+pedrosantosz@users.noreply.github.com>
2023-05-09 18:48:25 -0300

    Update ex04.c

--- Lista-5/ex04.c
+++ Lista-5/ex04.c
@@ -19,14 +19,10 @@ int main() {
 }
 
 int maiorNumero(int n1, int n2) {
-	int maior;
-	
 	if (n1 == n2)
-		maior = -1;
+		return -1;
 	else if (n1 > n2)
-		maior = n1;
+		return n1;
 	else
-		maior = n2;
-		
-	return maior;
+		return n2;
 }
